32ce30da51f0db72a3e9134ba94f45d844c40fa2
Kevin Huber <kevinahuber@gmail.com>
2017-10-22 21:45:02 -0500

    Add redux/router wrapper

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,35 +3,29 @@ import React from 'react'
 import {configure, addDecorator } from '@storybook/react'
 import { ConnectedRouter } from 'react-router-redux';
 import Header from '../src/header/index.js'
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store, { history } from '../src/store.js';
 
 const req = require.context('../stories', true, /\.js$/)
 
-// TODO: Wrap with redux and router
-// const provider = children => (
-//   <Provider store={store}>
-//     {children}
-//   </Provider>
-// )
-//
-// const router = children => (
-//   <ConnectedRouter history={history}>
-//     <div>
-//       <Route path='/' component={children} />
-//     </div>
-//   </ConnectedRouter>
-// )
-//
-// const layout = children => (
-//   <div>
-//     <Header />
-//     {children}
-//   </div>
-// )
-//
-// addDecorator(story => provider(router(layout(story()))))
+const layout = children => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <Header />
+        <div style={{paddingTop: 100}}>
+          {children}
+
+        </div>
+      </div>
+    </ConnectedRouter>
+  </Provider>
+)
+
+addDecorator(story => {
+  return layout(story())
+})
 
 function loadStories() {
   req.keys().forEach(file => req(file))
